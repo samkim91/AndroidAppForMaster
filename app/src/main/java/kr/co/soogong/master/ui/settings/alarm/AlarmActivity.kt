c432bf99e9fc59f116d77d83b2d11d94f97128cc
soogong namon <parksh@soogong.co.kr>
2020-09-17 21:41:34 +0900

    [AND-29] Setting 하위 Activity 생성

---
+++ app/src/main/java/kr/co/soogong/master/ui/settings/alarm/AlarmActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.soogong.master.ui.settings.alarm
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kr.co.soogong.master.R
+
+class AlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm)
+    }
+}
---
+++ app/src/main/java/kr/co/soogong/master/ui/settings/password/PasswordActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.soogong.master.ui.settings.password
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kr.co.soogong.master.R
+
+class PasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password)
+    }
+}
