941ef31b10842376ff41a4e48fb03a134c098e26
soogong-namon <parksh@soogong.co.kr>
2020-09-25 12:25:54 +0900

    [AND-30] Switch 디자인 변경

--- app/src/main/java/kr/co/soogong/master/ui/widget/TitleSwitchCompat.kt
+++ app/src/main/java/kr/co/soogong/master/ui/widget/TitleSwitchCompat.kt
@@ -5,76 +5,46 @@ import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.CompoundButton
-import android.widget.TextView
-import androidx.appcompat.widget.SwitchCompat
 import androidx.constraintlayout.widget.ConstraintLayout
 import kr.co.soogong.master.R
 import kr.co.soogong.master.databinding.ViewTitleSwitchcompatBinding
 
-class TitleSwitchCompat : ConstraintLayout {
-    private lateinit var titleTextView: TextView
-    private lateinit var switch: SwitchCompat
-    private lateinit var binding: ViewTitleSwitchcompatBinding
+class TitleSwitchCompat @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyle: Int = 0
+) : ConstraintLayout(context, attributeSet, defStyle) {
+    private var binding: ViewTitleSwitchcompatBinding =
+        ViewTitleSwitchcompatBinding.inflate(LayoutInflater.from(context), this, false)
 
-    constructor(context: Context) : super(context) {
-        initView()
-    }
-
-    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
-        initView()
-        getAttrs(attributeSet)
-    }
-
-    constructor(context: Context, attributeSet: AttributeSet, defStyle: Int) : super(
-        context,
-        attributeSet
-    ) {
-        initView()
+    init {
+        addView(binding.root)
         getAttrs(attributeSet, defStyle)
     }
 
-    private fun getAttrs(attributeSet: AttributeSet) {
-        val typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.TitleSwitchCompat)
-        setTypeArray(typedArray)
-    }
-
-    private fun initView() {
-        val infService = Context.LAYOUT_INFLATER_SERVICE;
-        val layoutInflater = context.getSystemService(infService) as LayoutInflater
-        binding = ViewTitleSwitchcompatBinding.inflate(layoutInflater, this, false)
-        addView(binding.root)
-
-        titleTextView = findViewById(R.id.title)
-        switch = findViewById(R.id.switch_compat)
-    }
-
-    private fun getAttrs(attributeSet: AttributeSet, defStyle: Int) {
+    private fun getAttrs(attributeSet: AttributeSet?, defStyle: Int) {
         val typedArray =
             context.obtainStyledAttributes(attributeSet, R.styleable.TitleSwitchCompat, defStyle, 0)
         setTypeArray(typedArray)
     }
 
     private fun setTypeArray(typedArray: TypedArray) {
-        titleTextView.text =
+        binding.title.text =
             typedArray.getString(R.styleable.TitleSwitchCompat_title_switch_title_text)
-        switch.isChecked =
+        binding.switchCompat.isChecked =
             typedArray.getBoolean(R.styleable.TitleSwitchCompat_title_switch_state, false)
         typedArray.recycle()
     }
 
     fun setTitleText(text: CharSequence) {
-        titleTextView.text = text
+        binding.title.text = text
     }
 
     fun setSwitchState(state: Boolean) {
-        switch.isChecked = state
-    }
-
-    fun getSwitchState(): Boolean {
-        return switch.isChecked
+        binding.switchCompat.isChecked = state
     }
 
     fun setSwitchClick(lister: CompoundButton.OnCheckedChangeListener) {
-        switch.setOnCheckedChangeListener(lister)
+        binding.switchCompat.setOnCheckedChangeListener(lister)
     }
 }
