401986a162f4629ebdbedf2dfea3a93cfd0a3f27
soogong-namon <parksh@soogong.co.kr>
2021-03-29 13:57:55 +0900

    Refactoring #2
    빠진 파일 추가

--- app/src/main/java/kr/co/soogong/master/ui/mypage/account/AccountViewModel.kt
+++ app/src/main/java/kr/co/soogong/master/ui/mypage/account/AccountViewModel.kt
@@ -1,14 +1,12 @@
 package kr.co.soogong.master.ui.mypage.account
 
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kr.co.soogong.master.network.AuthService
 import kr.co.soogong.master.ui.base.BaseViewModel
 import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class AccountViewModel @Inject constructor(
-    private val authService: AuthService
 ) : BaseViewModel() {
 
     fun withDraw() {
