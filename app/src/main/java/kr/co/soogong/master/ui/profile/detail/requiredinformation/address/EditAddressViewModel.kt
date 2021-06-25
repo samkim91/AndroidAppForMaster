a8a4f1146e49f196e378f7c37e51691318a7bccf
samkim91 <kimh616@gmail.com>
2021-06-25 16:02:43 +0900

    Refactor: Editting companyAddress logic

--- app/src/main/java/kr/co/soogong/master/ui/profile/detail/requiredinformation/address/EditAddressViewModel.kt
+++ app/src/main/java/kr/co/soogong/master/ui/profile/detail/requiredinformation/address/EditAddressViewModel.kt
@@ -5,7 +5,9 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
+import kr.co.soogong.master.data.dto.profile.MasterDto
 import kr.co.soogong.master.data.model.profile.CompanyAddress
+import kr.co.soogong.master.data.model.profile.Profile
 import kr.co.soogong.master.domain.usecase.profile.*
 import kr.co.soogong.master.ui.base.BaseViewModel
 import timber.log.Timber
@@ -13,25 +15,27 @@ import javax.inject.Inject
 
 @HiltViewModel
 class EditAddressViewModel @Inject constructor(
-    private val getCompanyAddressUseCase: GetCompanyAddressUseCase,
-    private val saveCompanyAddressUseCase: SaveCompanyAddressUseCase,
+    private val getProfileFromLocalUseCase: GetProfileFromLocalUseCase,
+    private val saveMasterUseCase: SaveMasterUseCase,
 ) : BaseViewModel() {
+    private val _profile = MutableLiveData<Profile>()
+
     val roadAddress = MutableLiveData("")
     val detailAddress = MutableLiveData("")
-    var latitude = MutableLiveData(0.0)
-    var longitude = MutableLiveData(0.0)
-
+    val latitude = MutableLiveData(0.0)
+    val longitude = MutableLiveData(0.0)
 
     fun requestCompanyAddress() {
         Timber.tag(TAG).d("requestCompanyAddress: ")
 
-        getCompanyAddressUseCase()
+        getProfileFromLocalUseCase()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeBy(
-                onSuccess = {
-                    roadAddress.postValue(it.roadAddress)
-                    detailAddress.postValue(it.detailAddress)
+                onSuccess = { profile ->
+                    _profile.value = profile
+                    roadAddress.postValue(profile.requiredInformation?.companyAddress?.roadAddress)
+                    detailAddress.postValue(profile.requiredInformation?.companyAddress?.detailAddress)
                 },
                 onError = { setAction(GET_COMPANY_ADDRESS_FAILED) }
             ).addToDisposable()
@@ -40,21 +44,21 @@ class EditAddressViewModel @Inject constructor(
     fun saveCompanyAddress() {
         Timber.tag(TAG).d("saveCompanyAddress: ")
 
-        roadAddress.value?.let {
-            saveCompanyAddressUseCase(
-                companyAddress = CompanyAddress(
-                    roadAddress = roadAddress.value!!,
-                    detailAddress = detailAddress.value ?: ""
-                ),
-                latitude = latitude.value!!,
-                longitude = longitude.value!!
-            ).subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeBy(
-                    onSuccess = { setAction(SAVE_COMPANY_ADDRESS_SUCCESSFULLY) },
-                    onError = { setAction(SAVE_COMPANY_ADDRESS_FAILED) }
-                ).addToDisposable()
-        }
+        saveMasterUseCase(
+            MasterDto(
+                id = _profile.value?.id,
+                uid = _profile.value?.uid,
+                roadAddress = roadAddress.value,
+                detailAddress = detailAddress.value,
+                latitude = latitude.value?.toFloat(),
+                longitude = longitude.value?.toFloat(),
+            )
+        ).subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onSuccess = { setAction(SAVE_COMPANY_ADDRESS_SUCCESSFULLY) },
+                onError = { setAction(SAVE_COMPANY_ADDRESS_FAILED) }
+            ).addToDisposable()
     }
 
     companion object {
