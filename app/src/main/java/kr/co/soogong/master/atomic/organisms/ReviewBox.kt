92abcc1720e5b99bca5d8bd7d3ee7a0cba5f4baa
EnterpriseDev <58809813+samkim91@users.noreply.github.com>
2021-11-18 17:01:47 +0900

    [Feat] adjusted reviewBox, profileFragment, buttons

--- app/src/main/java/kr/co/soogong/master/atomic/organisms/ReviewBox.kt
+++ app/src/main/java/kr/co/soogong/master/atomic/organisms/ReviewBox.kt
@@ -4,10 +4,8 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.res.ResourcesCompat
 import kr.co.soogong.master.R
 import kr.co.soogong.master.databinding.ViewReviewBoxBinding
-import kr.co.soogong.master.utility.extension.formatDecimal
 
 class ReviewBox @JvmOverloads constructor(
     context: Context,
@@ -17,53 +15,29 @@ class ReviewBox @JvmOverloads constructor(
     private val binding =
         ViewReviewBoxBinding.inflate(LayoutInflater.from(context), this, true)
 
-    var recommendCount: Double? = 0.0
+    var recommendCount: Double? = null
         set(value) {
             field = value
-            with(binding.tvRecommendCount) {
-                if (value == null || value == 0.0) {
-                    text = resources.getString(R.string.special_symbol_stroke)
-                    setTextColor(ResourcesCompat.getColor(resources, R.color.grey_1, null))
-                    return
-                }
-                text = value.formatDecimal()
-                setTextColor(ResourcesCompat.getColor(resources, R.color.brand_green, null))
+            value?.let {
+                binding.tvRecommendCount.text = it.toString()
             }
         }
 
-    var reviewCount: Int? = 0
+    var reviewCount: Int? = null
         set(value) {
             field = value
-            with(binding.tvReviewCount) {
-                if (value == null || value == 0) {
-                    text = resources.getString(R.string.special_symbol_stroke)
-                    setTextColor(ResourcesCompat.getColor(resources, R.color.grey_1, null))
-                    return
-                } else if (value > 1000) {
-                    text = resources.getString(R.string.over_one_thousand)
-                    setTextColor(ResourcesCompat.getColor(resources, R.color.brand_green, null))
-                    return
-                }
-                text = value.toString()
-                setTextColor(ResourcesCompat.getColor(resources, R.color.brand_green, null))
+            value?.let {
+                binding.tvReviewCount.text =
+                    if (it > 1000) resources.getString(R.string.over_one_thousand) else it.toString()
             }
         }
 
-    var completionCount: Int? = 0
+    var completionCount: Int? = null
         set(value) {
             field = value
-            with(binding.tvCompletionCount) {
-                if (value == null || value == 0) {
-                    text = resources.getString(R.string.special_symbol_stroke)
-                    setTextColor(ResourcesCompat.getColor(resources, R.color.grey_1, null))
-                    return
-                } else if (value > 1000) {
-                    text = resources.getString(R.string.over_one_thousand)
-                    setTextColor(ResourcesCompat.getColor(resources, R.color.brand_green, null))
-                    return
-                }
-                text = value.toString()
-                setTextColor(ResourcesCompat.getColor(resources, R.color.brand_green, null))
+            value?.let {
+                binding.tvCompletionCount.text =
+                    if (it > 1000) resources.getString(R.string.over_one_thousand) else it.toString()
             }
         }
 }
