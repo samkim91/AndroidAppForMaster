ebf6e2c06ef8ec441599dba6d1eff7f3b2f455fc
samkim91 <kimh616@gmail.com>
2021-11-29 14:33:40 +0900

    [Feat] new 2way databinding for SubheadlineChipGroup

--- app/src/main/java/kr/co/soogong/master/ui/profile/detail/businessunitinformation/EditBusinessUnitInformationFragment.kt
+++ app/src/main/java/kr/co/soogong/master/ui/profile/detail/businessunitinformation/EditBusinessUnitInformationFragment.kt
@@ -3,18 +3,13 @@ package kr.co.soogong.master.ui.profile.detail.businessunitinformation
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.children
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
-import com.google.android.material.chip.Chip
 import dagger.hilt.android.AndroidEntryPoint
 import gun0912.tedimagepicker.builder.TedImagePicker
 import kr.co.soogong.master.R
 import kr.co.soogong.master.atomic.molecules.SubheadlineChipGroup
-import kr.co.soogong.master.data.model.profile.ApprovedCodeTable
-import kr.co.soogong.master.data.model.profile.CorporateCodeTable
-import kr.co.soogong.master.data.model.profile.FreelancerCodeTable
-import kr.co.soogong.master.data.model.profile.SoleCodeTable
+import kr.co.soogong.master.data.model.profile.*
 import kr.co.soogong.master.databinding.FragmentEditBusinessUnitInformationBinding
 import kr.co.soogong.master.ui.base.BaseFragment
 import kr.co.soogong.master.ui.dialog.popup.CustomDialog
@@ -69,8 +64,6 @@ class EditBusinessUnitInformationFragment :
                     onDenied = { })
             }
 
-
-
             (activity as EditProfileContainerActivity).setSaveButtonClickListener {
                 viewModel.businessName.observe(viewLifecycleOwner, {
                     businessName.alertVisible = it.isNullOrEmpty()
@@ -88,7 +81,7 @@ class EditBusinessUnitInformationFragment :
                     alert.visibility = if (it == Uri.EMPTY) View.VISIBLE else View.GONE
                 })
 
-                if (viewModel.businessType.value == FreelancerCodeTable.code) {
+                if (viewModel.businessType.value == FreelancerCodeTable) {
                     if (shopName.alertVisible || businessNumber.alertVisible || alert.isVisible) return@setSaveButtonClickListener
                 } else {
                     if (businessName.alertVisible || shopName.alertVisible || businessNumber.alertVisible || alert.isVisible) return@setSaveButtonClickListener
@@ -112,46 +105,20 @@ class EditBusinessUnitInformationFragment :
     }
 
     private fun initChips() {
-        bind {
-
-            SubheadlineChipGroup
-                .initChips(requireContext(),
-                    layoutInflater,
-                    scgBusinessType,
-                    viewModel.businessTypes.value!!)
-
-            scgBusinessType.addCheckedChangeListener { group, position ->
-                when (position) {
-                    group.getChildAt(2).id -> {     // 프리랜서일 경우 레이아웃
-                        viewModel.businessType.value = FreelancerCodeTable.code
-                        businessName.isVisible = false
-                        shopName.subTitleVisible = true
-                        businessNumber.title = getString(R.string.requesting_birthday_label)
-                        businessRegistImageHolderLabel.text =
-                            getString(R.string.identical_image_holder_label)
-                    }
-                    else -> {       // 사업자일 경우 레이아웃
-                        viewModel.businessType.value =
-                            if (position == group.getChildAt(0).id) SoleCodeTable.code else CorporateCodeTable.code
-                        businessName.isVisible = true
-                        shopName.subTitleVisible = false
-                        businessNumber.title = getString(R.string.requesting_business_number_label)
-                        businessRegistImageHolderLabel.text =
-                            getString(R.string.business_identical_image_holder_label)
-                    }
-                }
-            }
-        }
+        SubheadlineChipGroup
+            .initChips(requireContext(),
+                layoutInflater,
+                binding.scgBusinessType,
+                viewModel.businessTypes.value?.map { it.inKorean }!!)
     }
 
     private fun registerEventObserve() {
         Timber.tag(TAG).d("registerEventObserve: ")
-        viewModel.businessType.observe(viewLifecycleOwner, { businessType ->
-            binding.scgBusinessType.container.children.forEach {
-                val chip = it as? Chip
-                if (chip?.text.toString() == businessType) chip?.isChecked = true
-            }
+
+        viewModel.businessType.observe(viewLifecycleOwner, { type ->
+            setLayoutByBusinessType(type)
         })
+
         viewModel.action.observe(viewLifecycleOwner, EventObserver { event ->
             when (event) {
                 SAVE_MASTER_SUCCESSFULLY -> {
@@ -162,6 +129,28 @@ class EditBusinessUnitInformationFragment :
                 }
             }
         })
+    }
+
+    private fun setLayoutByBusinessType(businessType: CodeTable) {
+        Timber.tag(TAG).d("setLayoutByBusinessType: ${businessType.code}")
+        with(binding) {
+            when (businessType) {
+                FreelancerCodeTable -> {        // 프리랜서일 경우 레이아웃
+                    businessName.isVisible = false
+                    shopName.subTitleVisible = true
+                    businessNumber.title = getString(R.string.requesting_birthday_label)
+                    businessRegistImageHolderLabel.text =
+                        getString(R.string.identical_image_holder_label)
+                }
+                else -> {       // 법인 또는 개인사업자일 경우 레이아웃
+                    businessName.isVisible = true
+                    shopName.subTitleVisible = false
+                    businessNumber.title = getString(R.string.requesting_business_number_label)
+                    businessRegistImageHolderLabel.text =
+                        getString(R.string.business_identical_image_holder_label)
+                }
+            }
+        }
     }
 
     companion object {
