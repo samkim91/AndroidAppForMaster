29a117ef30d0a7e5977e62420868c3a3a86c4747
samkim91 <kimh616@gmail.com>
2021-12-13 20:08:33 +0900

    [AND-120] renew PortfolioList activity

--- app/src/main/java/kr/co/soogong/master/uihelper/profile/EditProfileContainerFragmentHelper.kt
+++ app/src/main/java/kr/co/soogong/master/uihelper/profile/EditProfileContainerFragmentHelper.kt
@@ -8,7 +8,6 @@ import kr.co.soogong.master.ui.profile.detail.freemeasure.EditFreeMeasureFragmen
 import kr.co.soogong.master.ui.profile.detail.introduction.EditIntroductionFragment
 import kr.co.soogong.master.ui.profile.detail.major.EditMajorFragment
 import kr.co.soogong.master.ui.profile.detail.masterconfig.EditMasterConfigFragment
-import kr.co.soogong.master.ui.profile.detail.ownername.EditOwnerNameFragment
 import kr.co.soogong.master.ui.profile.detail.phonenumber.EditPhoneNumberFragment
 import kr.co.soogong.master.ui.profile.detail.portfoliolist.portfolio.PortfolioFragment
 import kr.co.soogong.master.ui.profile.detail.portfoliolist.pricebyproject.PriceByProjectFragment
@@ -18,23 +17,23 @@ import kr.co.soogong.master.ui.profile.detail.warranty.EditWarrantyInformationFr
 object EditProfileContainerFragmentHelper {
     const val TAG = "EditProfileContainerFragmentHelper"
 
+    // 필수 정보
+    const val EDIT_INTRODUCTION = "업체 소개"
+    const val EDIT_SHOP_IMAGES = "대표 이미지"
+    const val EDIT_BUSINESS_UNIT_INFORMATION = "사업자 정보"
+    const val EDIT_WARRANTY_INFORMATION = "A/S 정보"
+    const val EDIT_PHONE_NUMBER = "휴대폰 번호"
+    const val EDIT_MAJOR = "시공 가능 업종"
+    const val EDIT_ADDRESS = "활동 지역"
+
+    // 선택 정보
     const val FREE_MEASURE = "무료 방문 실측 여부 수정하기"
     const val EDIT_PORTFOLIO = "포트폴리오 수정하기"
     const val ADD_PORTFOLIO = "포트폴리오 추가하기"
     const val EDIT_PRICE_BY_PROJECTS = "시공 종류별 가격 수정하기"
     const val ADD_PRICE_BY_PROJECTS = "시공 종류별 가격 추가하기"
-
-    const val EDIT_EMAIL = "이메일 등록·수정하기"
-
-    const val EDIT_INTRODUCTION = "업체 소개"
-    const val EDIT_SHOP_IMAGES = "대표 이미지"
-    const val EDIT_BUSINESS_UNIT_INFORMATION = "사업자 정보"
-    const val EDIT_WARRANTY_INFORMATION = "A/S 정보"
-    const val EDIT_OWNER_NAME = "대표자명 등록·수정하기"
-    const val EDIT_PHONE_NUMBER = "휴대폰 번호"
-    const val EDIT_ADDRESS = "활동 지역"
-    const val EDIT_MAJOR = "시공 가능 업종"
     const val EDIT_MASTER_CONFIG = "기타 변동 요인"
+    const val EDIT_EMAIL = "이메일 등록·수정하기"
 
     fun getFragment(pageName: String, itemId: Int?): Fragment =
         when (pageName) {
@@ -53,7 +52,6 @@ object EditProfileContainerFragmentHelper {
             EDIT_SHOP_IMAGES -> EditShopImagesFragment.newInstance()
             EDIT_BUSINESS_UNIT_INFORMATION -> EditBusinessUnitInformationFragment.newInstance()
             EDIT_WARRANTY_INFORMATION -> EditWarrantyInformationFragment.newInstance()
-            EDIT_OWNER_NAME -> EditOwnerNameFragment.newInstance()
             EDIT_PHONE_NUMBER -> EditPhoneNumberFragment.newInstance()
             EDIT_MAJOR -> EditMajorFragment.newInstance()
             EDIT_ADDRESS -> EditAddressFragment.newInstance()
