6a650bf11a43c1f7832e808c2665a54255ec67cc
samkim91 <kimh616@gmail.com>
2021-12-30 19:08:14 +0900

    [Fix] signUp bug, home fragment, profile fragment, status bar

--- app/src/main/java/kr/co/soogong/master/ui/auth/signup/steps/MajorFragment.kt
+++ app/src/main/java/kr/co/soogong/master/ui/auth/signup/steps/MajorFragment.kt
@@ -6,16 +6,14 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.fragment.app.activityViewModels
+import com.google.android.material.chip.Chip
 import dagger.hilt.android.AndroidEntryPoint
 import kr.co.soogong.master.R
-import kr.co.soogong.master.data.model.major.Major
 import kr.co.soogong.master.databinding.FragmentSignUpMajorBinding
 import kr.co.soogong.master.ui.auth.signup.SignUpActivity
 import kr.co.soogong.master.ui.auth.signup.SignUpViewModel
 import kr.co.soogong.master.ui.base.BaseFragment
 import kr.co.soogong.master.uihelper.major.MajorActivityHelper
-import kr.co.soogong.master.uihelper.major.MajorActivityHelper.BUNDLE_MAJOR
-import kr.co.soogong.master.utility.MajorChipGroupHelper
 import timber.log.Timber
 
 @AndroidEntryPoint
@@ -28,16 +26,22 @@ class MajorFragment : BaseFragment<FragmentSignUpMajorBinding>(
         registerForActivityResult(StartActivityForResult()) { result ->
             Timber.tag(TAG).d("StartActivityForResult: $result")
             if (result.resultCode == Activity.RESULT_OK) {
-                val data: Intent? = result.data
-                val selectedMajor: Major by lazy {
-                    data?.getParcelableExtra(BUNDLE_MAJOR) ?: Major(null, null)
+//                val data: Intent? = result.data
+//                val selectedMajor: Major by lazy {
+//                    data?.getParcelableExtra(BUNDLE_MAJOR) ?: Major(null, null)
+//                }
+//                MajorChipGroupHelper.makeEntryChipGroupWithSubtitleForMajor(
+//                    layoutInflater = layoutInflater,
+//                    container = binding.majorContainer,
+//                    newMajor = selectedMajor,
+//                    viewModelBusinessTypes = viewModel.majors
+//                )
+
+                result.data?.let { intent ->
+                    MajorActivityHelper.getProjectsFromIntent(intent)?.let { projects ->
+                        viewModel.projects.addAll(projects)
+                    }
                 }
-                MajorChipGroupHelper.makeEntryChipGroupWithSubtitleForMajor(
-                    layoutInflater = layoutInflater,
-                    container = binding.majorContainer,
-                    newMajor = selectedMajor,
-                    viewModelBusinessTypes = viewModel.majors
-                )
             }
         }
 
@@ -46,6 +50,7 @@ class MajorFragment : BaseFragment<FragmentSignUpMajorBinding>(
         super.onViewCreated(view, savedInstanceState)
         Timber.tag(TAG).d("onViewCreated: ")
         initLayout()
+        registerEventObserve()
     }
 
     override fun initLayout() {
@@ -66,7 +71,8 @@ class MajorFragment : BaseFragment<FragmentSignUpMajorBinding>(
             }
 
             defaultButton.setOnClickListener {
-                viewModel.majors.observe(viewLifecycleOwner, {
+                viewModel.projects.observe(viewLifecycleOwner, {
+                    Timber.tag(TAG).d("defaultButton: ${it.size}")
                     major.alertVisible = it.isNullOrEmpty()
                 })
 
@@ -76,6 +82,26 @@ class MajorFragment : BaseFragment<FragmentSignUpMajorBinding>(
                 }
             }
         }
+    }
+
+    private fun registerEventObserve() {
+        Timber.tag(TAG).d("registerEventObserve: ")
+        viewModel.projects.observe(viewLifecycleOwner, { projects ->
+            binding.cgContainer.removeAllViews()        // update 가 일어날 때마다, 계속 chip 이 추가되기에 초기에 전체삭제
+
+            projects.map { project ->
+                binding.cgContainer.addView(
+                    (layoutInflater.inflate(R.layout.chip_entry_layout,
+                        binding.cgContainer,
+                        false) as Chip).also { chip ->
+                        chip.text = project.name
+                        chip.setOnCloseIconClickListener {
+                            viewModel.projects.remove(project)
+                        }
+                    }
+                )
+            }
+        })
     }
 
     companion object {
--- app/src/main/java/kr/co/soogong/master/ui/profile/detail/email/EditEmailViewModel.kt
+++ app/src/main/java/kr/co/soogong/master/ui/profile/detail/email/EditEmailViewModel.kt
@@ -2,7 +2,7 @@ package kr.co.soogong.master.ui.profile.detail.email
 
 import androidx.lifecycle.MutableLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kr.co.soogong.master.data.common.DropdownItems
+import kr.co.soogong.master.data.common.DropdownItemList
 import kr.co.soogong.master.data.dto.profile.MasterDto
 import kr.co.soogong.master.domain.usecase.profile.GetProfileUseCase
 import kr.co.soogong.master.domain.usecase.profile.SaveMasterUseCase
@@ -16,7 +16,7 @@ class EditEmailViewModel @Inject constructor(
     saveMasterUseCase: SaveMasterUseCase,
 ) : EditProfileContainerViewModel(getProfileUseCase, saveMasterUseCase) {
 
-    val domains = DropdownItems.domains
+    val domains = DropdownItemList.domains
     val localPart = MutableLiveData("")
     val domain = MutableLiveData<Pair<String, Int>>()
 
