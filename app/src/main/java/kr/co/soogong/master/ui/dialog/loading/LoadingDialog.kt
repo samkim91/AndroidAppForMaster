e834c129221293f90fa263f009f775fcb2b32436
samkim91 <kimh616@gmail.com>
2022-01-26 17:05:55 +0900

    [Fix] Loading is not cancelable

--- app/src/main/java/kr/co/soogong/master/ui/dialog/loading/LoadingDialog.kt
+++ app/src/main/java/kr/co/soogong/master/ui/dialog/loading/LoadingDialog.kt
@@ -1,13 +1,14 @@
 package kr.co.soogong.master.ui.dialog.loading
 
 import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
 import androidx.fragment.app.DialogFragment
-import kr.co.soogong.master.R
 import kr.co.soogong.master.databinding.DialogLoadingBinding
 import timber.log.Timber
 
@@ -26,22 +27,22 @@ class LoadingDialog : DialogFragment() {
     ): View {
         Timber.tag(TAG).d("onCreateView: ")
         binding = DialogLoadingBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        Timber.tag(TAG).d("onCreateDialog: ")
-        return super.onCreateDialog(savedInstanceState).apply {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        super.onCreateDialog(savedInstanceState).apply {
+            Timber.tag(TAG).d("onCreateDialog: ")
+
             requestWindowFeature(Window.FEATURE_NO_TITLE)
-            window?.setBackgroundDrawableResource(R.color.transparent)
+            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            isCancelable = false
         }
-    }
 
     companion object {
         private const val TAG = "LoadingDialog"
 
-        fun newInstance(): LoadingDialog {
-            return LoadingDialog()
-        }
+        fun newInstance() = LoadingDialog()
     }
 }
