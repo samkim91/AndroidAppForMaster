01b4d36943cddc2754871d5ad0abb35c8a7c8ec9
samkim91 <kimh616@gmail.com>
2022-02-07 21:18:05 +0900

    [Feat] refactoring RequirementFilteredPager

--- app/src/main/java/kr/co/soogong/master/ui/requirement/list/filter/RequirementsFilterPagerAdapter.kt
+++ app/src/main/java/kr/co/soogong/master/ui/requirement/list/filter/RequirementsFilterPagerAdapter.kt
@@ -2,15 +2,18 @@ package kr.co.soogong.master.ui.requirement.list.filter
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import kr.co.soogong.master.data.model.requirement.RequirementStatus
 import kr.co.soogong.master.ui.requirement.list.TAB_TEXTS_REQUIREMENTS_BEFORE_PROGRESS
 import kr.co.soogong.master.ui.requirement.list.TAB_TEXTS_REQUIREMENTS_IN_PROGRESS
 
 class RequirementsFilterPagerAdapter(
     fragment: Fragment,
-    private val type: Int,
+    private val mainTabIndex: Int,
 ) : FragmentStateAdapter(fragment) {
     override fun getItemCount() =
-        if (type == 0) TAB_TEXTS_REQUIREMENTS_BEFORE_PROGRESS.count() else TAB_TEXTS_REQUIREMENTS_IN_PROGRESS.count()
+        if (mainTabIndex == 0) TAB_TEXTS_REQUIREMENTS_BEFORE_PROGRESS.count() else TAB_TEXTS_REQUIREMENTS_IN_PROGRESS.count()
 
-    override fun createFragment(position: Int) = RequirementsFilterFragment.newInstance()
+    override fun createFragment(position: Int) =
+        RequirementsFilterFragment.newInstance(
+            RequirementStatus.getRequirementStatusFromTabIndex(mainTabIndex, position))
 }
