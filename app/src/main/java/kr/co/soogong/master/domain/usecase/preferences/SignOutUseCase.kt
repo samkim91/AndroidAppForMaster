6648b7c0523b69070973a97c563b73f4a92b6497
soogong-myeonghwan <myeonghwan@soogong.co.kr>
2022-08-23 12:04:17 +0900

    [Fix] warning: com.google.android.gms:play-services-safetynet

--- app/src/main/java/kr/co/soogong/master/domain/usecase/preferences/SignOutUseCase.kt
+++ app/src/main/java/kr/co/soogong/master/domain/usecase/preferences/SignOutUseCase.kt
@@ -17,10 +17,6 @@ class SignOutUseCase @Inject constructor(
     // TODO: 2022/02/16 repository 들을 생성자로 가져와서, 삭제하는 것으로 변경 필요 !!
 
     operator fun invoke() {
-        // sign out 을 하면 firebase auth 의 해당 계정 식별자가 reset 되는 문제가 있어서,
-        // 당분간 shared preferences 로 로그인 여부를 확인하는 것으로 유지
-//        Firebase.auth.signOut()
-
         requirementDao.removeAll()
         masterDao.removeAll()
         noticeDao.removeAll()
